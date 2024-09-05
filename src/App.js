aed5a1a1ca85a6c011d445dea299c5cb4602890e
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-09-05 16:07:00 -0500

    Crear botones

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import { Boton } from './components/boton/boton';
 import freeCodeCampLogo from './img/freecodecamp-logo.png'
 
 function App() {
   return (
     <div className="App">
-      <div className='freecodecamp__logo__contenedor'>
-        <img className='freecodecamp__logo' src={freeCodeCampLogo} alt='Logo de freeCodeCamp' />
-      </div>
+        <div className='freecodecamp__logo__contenedor'>
+            <img className='freecodecamp__logo' src={freeCodeCampLogo} alt='Logo de freeCodeCamp' />
+        </div>
+        <div className='contenedor__principal'>
+            <Boton texto='Clic' botonClic={true} />
+            <Boton texto='Reiniciar' botonClic={false} />
+        </div>
     </div>
   );
 }
---
+++ src/components/boton/boton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Boton({ texto, botonClic, manejarClic }){
+    return(
+        <button className={botonClic ? 'boton__clic' : 'boton__reiniciar'} onClick={manejarClic}>
+            {texto}
+        </button>
+    );
+}
+
+export { Boton };
