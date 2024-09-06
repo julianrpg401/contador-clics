7fad35f069094d51ceb511f279467c1c25ef2892
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-09-06 14:17:09 -0500

    Add new files

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import { Boton } from './components/boton/boton';
 import freeCodeCampLogo from './img/freecodecamp-logo.png'
 
 function App() {
+  const manejarClic = () => {
+    console.log('Clic');
+  }
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+  }
+
   return (
     <div className="App">
         <div className='freecodecamp__logo__contenedor'>
             <img className='freecodecamp__logo' src={freeCodeCampLogo} alt='Logo de freeCodeCamp' />
         </div>
         <div className='contenedor__principal'>
-            <Boton texto='Clic' botonClic={true} />
-            <Boton texto='Reiniciar' botonClic={false} />
+            <Boton texto='Clic' botonClic={true} manejarClic={manejarClic} />
+            <Boton texto='Reiniciar' botonClic={false} manejarClic={reiniciarContador} />
         </div>
     </div>
   );
